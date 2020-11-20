bb7a5c7413858ca74da2c7477ceac5cbcac5a700
mainR <mainr@debian.debian>
2020-11-20 14:43:49 +0100

    commit operation

---
+++ binaryPrintForm/binaryPrintForm/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace binaryPrintForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mainBox = new System.Windows.Forms.PictureBox();
+            this.textBoxStringText = new System.Windows.Forms.TextBox();
+            this.btnMinimize = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.textBoxBooleanText = new System.Windows.Forms.TextBox();
+            this.btnDraw = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.mainBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // mainBox
+            // 
+            this.mainBox.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.mainBox.Location = new System.Drawing.Point(20, 100);
+            this.mainBox.Name = "mainBox";
+            this.mainBox.Size = new System.Drawing.Size(1280, 960);
+            this.mainBox.TabIndex = 1;
+            this.mainBox.TabStop = false;
+            // 
+            // textBoxStringText
+            // 
+            this.textBoxStringText.AllowDrop = true;
+            this.textBoxStringText.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.textBoxStringText.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBoxStringText.Location = new System.Drawing.Point(1320, 500);
+            this.textBoxStringText.MaxLength = 0;
+            this.textBoxStringText.Multiline = true;
+            this.textBoxStringText.Name = "textBoxStringText";
+            this.textBoxStringText.ReadOnly = true;
+            this.textBoxStringText.Size = new System.Drawing.Size(580, 250);
+            this.textBoxStringText.TabIndex = 2;
+            this.textBoxStringText.Text = "textBoxStringText";
+            // 
+            // btnMinimize
+            // 
+            this.btnMinimize.Location = new System.Drawing.Point(1752, 12);
+            this.btnMinimize.Name = "btnMinimize";
+            this.btnMinimize.Size = new System.Drawing.Size(75, 23);
+            this.btnMinimize.TabIndex = 3;
+            this.btnMinimize.Text = "_";
+            this.btnMinimize.UseVisualStyleBackColor = true;
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(1833, 12);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 4;
+            this.btnClose.Text = "X";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // textBoxBooleanText
+            // 
+            this.textBoxBooleanText.AllowDrop = true;
+            this.textBoxBooleanText.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.textBoxBooleanText.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBoxBooleanText.Location = new System.Drawing.Point(1320, 100);
+            this.textBoxBooleanText.MaxLength = 0;
+            this.textBoxBooleanText.Multiline = true;
+            this.textBoxBooleanText.Name = "textBoxBooleanText";
+            this.textBoxBooleanText.ReadOnly = true;
+            this.textBoxBooleanText.Size = new System.Drawing.Size(580, 250);
+            this.textBoxBooleanText.TabIndex = 5;
+            this.textBoxBooleanText.Text = "textBoxBooleanText";
+            // 
+            // btnDraw
+            // 
+            this.btnDraw.Location = new System.Drawing.Point(1306, 938);
+            this.btnDraw.Name = "btnDraw";
+            this.btnDraw.Size = new System.Drawing.Size(602, 122);
+            this.btnDraw.TabIndex = 6;
+            this.btnDraw.Text = "DRAW";
+            this.btnDraw.UseVisualStyleBackColor = true;
+            this.btnDraw.Click += new System.EventHandler(this.btnDraw_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(1307, 890);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(1920, 1080);
+            this.ControlBox = false;
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnDraw);
+            this.Controls.Add(this.textBoxBooleanText);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnMinimize);
+            this.Controls.Add(this.textBoxStringText);
+            this.Controls.Add(this.mainBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            ((System.ComponentModel.ISupportInitialize)(this.mainBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox mainBox;
+        private System.Windows.Forms.TextBox textBoxStringText;
+        private System.Windows.Forms.Button btnMinimize;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.TextBox textBoxBooleanText;
+        private System.Windows.Forms.Button btnDraw;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ binaryPrintForm/binaryPrintForm/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace binaryPrintForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            //string plainText = "Capitalism is the perfect recipe for life long of sadness. And I like very thicc pussy ass bitches. jk i'm an incel, but i'm gonna say it twice because the string is not long enough";
+            string plainText = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit cursus nunc,Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit cursus nunc, Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ncidunt tempus.";
+            string binaryText = StringToBinary(plainText);
+            string stringText = BinaryToString(binaryText);
+            textBoxBooleanText.Text = binaryText;
+            textBoxStringText.Text = stringText;
+            char[] binaryChars = binaryText.ToCharArray();
+
+            //-----------Actually doing it :)
+
+            try
+            {
+                int width = 1280;
+                int height = 960;
+                // Retrieve the image.
+                Bitmap image = new Bitmap(width, height);
+                bool flag = true;
+
+                int i = 0;
+                // Loop through the images pixels to reset color.
+                for (int y = 10; y < image.Width && flag; y += 2)
+                {
+
+                    for (int x = 10; x < image.Height; x++)
+                    {
+                        if (i < binaryChars.Length)
+                        {
+                            if (binaryChars[i] == '1')
+                            {
+
+                                Color pixelColor = image.GetPixel(x, y);
+                                Color newColor = Color.FromArgb(pixelColor.R, 0, 0);
+                                image.SetPixel(x, y, newColor);
+                            }/*
+                            else
+                            {
+                                Color pixelColor = image.GetPixel(x, y);
+                                Color newColor = Color.FromArgb(pixelColor.R, 255, 0, 255);
+                                image.SetPixel(x, y, newColor);
+                            }*/
+
+                            i++;
+                        }
+                    }
+                }
+                // Set the PictureBox to display the image.
+                mainBox.Image = image;
+
+                // Display the pixel format in Label1.
+                label1.Text = "Pixel format: " + image.PixelFormat.ToString();
+            }
+            catch (ArgumentException)
+            {
+                MessageBox.Show("There was an error." +
+                    "Check the path to the image file.");
+            }
+
+            //-----------Untill here :)
+
+        }
+        //CONVERT A PLAIN TEXT STRING TO BINARY-STRING
+        public static string StringToBinary(string data)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            foreach (char c in data.ToCharArray())
+            {
+                sb.Append(Convert.ToString(c, 2).PadLeft(8, '0'));
+            }
+            return sb.ToString();
+        }
+        //CONVERT A BINARY-STRING TO PLAIN TEXT
+        public static string BinaryToString(string data)
+        {
+            List<Byte> byteList = new List<Byte>();
+
+            for (int i = 0; i < data.Length; i += 8)
+            {
+                byteList.Add(Convert.ToByte(data.Substring(i, 8), 2));
+            }
+            return Encoding.ASCII.GetString(byteList.ToArray());
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnDraw_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                int width = 100;
+                int height = 100;
+                // Retrieve the image.
+                Bitmap image = new Bitmap(width,height);
+
+
+                // Loop through the images pixels to reset color.
+                for (int x = 0; x < image.Width; x++)
+                {
+                    for (int y = 0; y < image.Height; y++)
+                    {
+                        Color pixelColor = image.GetPixel(x, y);
+                        Color newColor = Color.FromArgb(pixelColor.R, 0, 0);
+                        image.SetPixel(x, y, newColor);
+                    }
+                }
+
+                // Set the PictureBox to display the image.
+                mainBox.Image = image;
+
+                // Display the pixel format in Label1.
+                label1.Text = "Pixel format: " + image.PixelFormat.ToString();
+            }
+            catch (ArgumentException)
+            {
+                MessageBox.Show("There was an error." +
+                    "Check the path to the image file.");
+            }
+        }
+    }
+}
